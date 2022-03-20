5b450e7a2b18845cd84fce32e4a20b5c38497abf
mertmutlu <mertmutlu33@gmail.com>
2022-03-20 20:15:00 +0300

    fetch single episode in character detail

--- src/pages/CharacterDetail/CharacterDetail.js
+++ src/pages/CharacterDetail/CharacterDetail.js
@@ -8,10 +8,23 @@ import "./CharacterDetail.css";
 function CharacterDetail() {
   const { characterId } = useParams();
   const [character, setCharacter] = useState();
+  const [episodes, setEpisodes] = useState([]);
+  const [episodeDetail, setEpisodeDetail] = useState();
+
+  const [trigger, setTrigger] = useState(1);
 
   useEffect(() => {
+    console.log("first use eff");
     getCharacter();
   }, []);
+
+  useEffect(() => {
+    console.log("second use eff");
+    console.log(character);
+    if (character) {
+      getEpisodes();
+    }
+  }, [character, trigger]);
 
   const getCharacter = async () => {
     const response = await axios.get(
@@ -20,6 +33,15 @@ function CharacterDetail() {
 
     setCharacter(response.data);
   };
+
+  const getEpisodes = async () => {
+    setEpisodes(character.episode);
+
+    const episodeDetail = await axios.get(`${character.episode[0]}`);
+    setEpisodeDetail(episodeDetail.data.episode);
+    console.log(episodeDetail.data.episode);
+  };
+  // console.log(episodes);
 
   if (character?.id == null) {
     return <p>loading</p>;
@@ -32,6 +54,14 @@ function CharacterDetail() {
         Selected Character Id, {characterId}, {character.name}
       </p>
       <img src={character.image} alt="characterImage" />
+      <p>Episode Detail {episodeDetail}</p>
+      <button
+        onClick={() => {
+          setTrigger(trigger + 1);
+        }}
+      >
+        trigger
+      </button>
     </div>
   );
 }
